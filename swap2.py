cc0a870aa866cbcda5e03c4e410a20b441cb5402
kamalesh.B <82257435+kamalesh20-hacker@users.noreply.github.com>
2021-04-14 11:58:47 +0530

    Add files via upload

---
+++ swap2.py
@@ -0,0 +1,13 @@
+a=int(input("Enter the First number:"))
+b=int(input("Enter the Second number:"))
+a,b=b,a
+print("After swapping Numbers")
+print("The value of a is:",a)
+print("The value of b is :",b)
+
+#output:
+#Enter the First number:20
+#Enter the Second number:10
+#After swapping Numbers
+#The value of a is: 10
+#The value of b is : 20
